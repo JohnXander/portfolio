318ed0ca076f335383ee65e026e7bb87e6dabb3a
John Xander <johnblox5@gmail.com>
2022-11-10 22:52:49 +0100

    add style to navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,31 +9,34 @@ const Header = ({ sections }) => {
     }
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark border-bottom border-secondary p-4">
+        <nav
+            className="navbar navbar-expand-lg navbar-dark bg-dark pt-4 px-3"
+            style={{ paddingBottom: '0' }}
+        >
             <ul className="navbar-nav">
                 <li
                     className="nav-item nav-link"
                     onClick={() => scrollToSection(sections[0])}
-                    style={{ cursor: 'pointer' }}
+                    style={{ cursor: 'pointer', fontSize: '1.25rem', paddingRight: '1.25rem' }}
                 >
                     Profile
                 </li>
                 <li
                     className="nav-item nav-link"
                     onClick={() => scrollToSection(sections[1])}
-                    style={{ cursor: 'pointer' }}
+                    style={{ cursor: 'pointer', fontSize: '1.25rem', paddingRight: '1.25rem' }}
                 >
                     Projects
                 </li>
                 <li
                     className="nav-item nav-link"
                     onClick={() => scrollToSection(sections[2])}
-                    style={{ cursor: 'pointer' }}
+                    style={{ cursor: 'pointer', fontSize: '1.25rem', paddingRight: '1.25rem' }}
                 >
                     Background
                 </li>
             </ul>
-        </nav>
+        </nav >
     )
 }
 
